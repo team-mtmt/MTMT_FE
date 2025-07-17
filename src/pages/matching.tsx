32e7add35f7ec205f804b032fd4233deeab50a64
이의진 <aghpt04@gmail.com>
2025-07-17 15:47:05 +0900

    feat

--- src/pages/matching.tsx
+++ src/pages/matching.tsx
@@ -4,22 +4,19 @@ import { useState } from "react";
 import RatingImg from "../assets/imgs/ratings/veryGood.svg";
 import { Tag } from "../components/tag";
 import { Link } from "react-router-dom";
-import { getMatchingMento } from "../apis";
+import axios from "axios";
 import { useEffect } from "react";
-import { categoryLabels } from "../apis/type";
-import { ratingIcons } from "../apis/type";
-import { MentorsResponse } from "../apis/type";
 
 export const Matching = () => {
     const [selected, setSelected] = useState<number | 0>(0);
-    const [mentors, setMentors] = useState<MentorsResponse>({});
+    const [mentors, setMentors] = useState<{ [key: string]: any[] }>({});
 
     const categories = Object.keys(mentors);
 
     useEffect(() => {
-        const fetchMentors = async () => {
+    const fetchMentors = async () => {
         try {
-        const res = await getMatchingMento();
+        const res = await axios.get("/matching");
         setMentors(res.data);
         } catch (error) {
         console.error(error);
@@ -29,30 +26,8 @@ export const Matching = () => {
     fetchMentors();
     }, []);
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const token = urlParams.get("token");
-    
-        if (token) {
-          localStorage.setItem("token", token);
-          console.log("토큰 저장됨:", token);
-        }
-    }, []);
-
     const selectedCategory = categories[selected] || "";
     const selectedMentors = mentors[selectedCategory] || [];
-
-    const getRealImgUrl = (url: string | null) => {
-        if (!url) return RatingImg; // 기본 이미지 대체
-      
-        // Imgur 링크인 경우
-        if (url.includes("imgur.com") && !url.includes("i.imgur.com")) {
-          const id = url.split("/").pop(); // "54lBcBj"
-          return `https://i.imgur.com/${id}.jpg`;
-        }
-      
-        return url;
-    };
 
     return (
         <Wrapper>
@@ -61,38 +36,48 @@ export const Matching = () => {
                 <Text variant="Stitle" weight={700}>딱 맞는 멘토분들을 찾았어요!</Text>
             </TextWrapper>
             <SelectBarWrapper>
-                {categories.map((category, idx) => (
-                        <SelectButton
-                        $selected={selected === idx}
-                        key={category}
-                        onClick={() => setSelected(idx)}
-                        >
-                        <Text color={selected === idx ? "black" : "gray.500"} variant="Caption">
-                            {categoryLabels[category] ?? category}
-                        </Text>
-                        </SelectButton>
-                    )
-                )}
+                {categories.map((content, idx) => (
+                    <SelectButton $selected={selected === idx} key={idx} onClick={() => setSelected(idx)}>
+                        <Text color={selected === idx ? 'black' : 'gray.500'} variant="Caption">{content}</Text>
+                    </SelectButton>
+                ))}
             </SelectBarWrapper>
             <ContentBoxWrapper>
-            {selectedMentors.map((mentor) => (
-            <Link to={`/profile/${mentor.id}`} key={mentor.id}>
-              <ContentBox>
-                <Picture src={getRealImgUrl(mentor.thumbnail)}/>
-                <ContentWrapper>
-                  <NameWrapper>
-                    <Text variant="Caption">{mentor.name}</Text>
-                    <img src={ratingIcons[mentor.rating_section] ?? RatingImg} alt={`${mentor.rating_section} rating`} />
-                  </NameWrapper>
-                  <Text color="gray.700" variant="Button">
-                    {mentor.location}
-                  </Text>
-                  <Tag />
-                </ContentWrapper>
-              </ContentBox>
-            </Link>
-          )
-        )}
+                <Link to={'/profile'}>
+                    <ContentBox>
+                        <Picture />
+                        <ContentWrapper>
+                            <NameWrapper>
+                                <Text variant="Caption">손희찬</Text>
+                                <img src={RatingImg} alt="" />
+                            </NameWrapper>
+                            <Text color="gray.700" variant="Button">대전광역시, 중구</Text>
+                            <Tag />
+                        </ContentWrapper>
+                    </ContentBox>
+                </Link>
+                <ContentBox>
+                    <Picture />
+                    <ContentWrapper>
+                        <NameWrapper>
+                            <Text variant="Caption">손희찬</Text>
+                            <img src={RatingImg} alt="" />
+                        </NameWrapper>
+                        <Text color="gray.700" variant="Button">대전광역시, 중구</Text>
+                        <Tag />
+                    </ContentWrapper>
+                </ContentBox>
+                <ContentBox>
+                    <Picture />
+                    <ContentWrapper>
+                        <NameWrapper>
+                            <Text variant="Caption">손희찬</Text>
+                            <img src={RatingImg} alt="" />
+                        </NameWrapper>
+                        <Text color="gray.700" variant="Button">대전광역시, 중구</Text>
+                        <Tag />
+                    </ContentWrapper>
+                </ContentBox>
             </ContentBoxWrapper>
         </Wrapper>
     )
@@ -138,9 +123,10 @@ const ContentBox = styled.div`
     gap: 12px;
 `;
 
-const Picture = styled.img`
+const Picture = styled.div`
     width: 60px;
     height: 60px;
+    background-color: white;
     border: 0.2px solid ${({theme}) => theme.colors.gray[300]};
     border-radius: 4px;
 `;
